2dda14504a2d0db5c9bf126ae58b3581b8d4d5ff
alamgirweb11 <alamgirweb11@gmail.com>
2022-11-05 21:50:39 +0600

    install and configure vuex

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,13 @@
 <template>
     <div>
-        <h1 class="text-center text-purple-500">Welcome to home page!</h1>
+        <h1 class="text-center text-purple-500">{{ meals }}</h1>
     </div>
 </template>
-<script>
+<script setup>
+import { computed } from 'vue'
+import store from '../store'
+
+const meals = computed(() => store.state.meals)
 
 </script>
 <style>
