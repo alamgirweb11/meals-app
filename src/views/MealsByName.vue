d4be4d84c95f24efd7b4fe41d0a566c07f9c9984
alamgirweb11 <alamgirweb11@gmail.com>
2022-12-18 17:42:11 +0600

    fetch seach by name meals

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,12 +1,32 @@
 <template>
-    <div>
-        By Name
-    </div>
+     <div class="p-8">
+        <input type="text"
+        v-model="keyword"
+        class="rounded border-2 border-gray-200 w-full"
+        placeholder="Search for meals"
+        @change="searchMeals"
+        >
+        <br>
+        <pre>{{ meals }}</pre>
+       </div>
 </template>
-<script>
-export default {
-    
-}
+<script setup>
+ import { computed } from '@vue/reactivity';
+ import { ref } from 'vue';
+ import store from '../store';
+
+ const meals = computed(() => store.state.searchedMeals)
+
+ const keyword = ref("")
+
+ function searchMeals() {
+      if(keyword.value){
+       store.dispatch('searchMeals', keyword.value)
+     } else {
+        store.dispatch('searchMeals', [])
+     }
+ }
+
 </script>
 <style>
     
