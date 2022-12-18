d1a99ae9c132ab33350d1d914405a8716d6f9396
alamgirweb11 <alamgirweb11@gmail.com>
2022-12-19 01:52:14 +0600

    make meal search card to component

---
+++ src/components/MealItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="bg-white shadow rounded-xl">
+    <router-link :to="{ name: 'mealDetails', params: { id: meal.idMeal } }">
+      <img
+        :src="meal.strMealThumb"
+        :alt="meal.strMeal"
+        class="w-full h-48 object-cover rounded-t-2xl"
+      />
+    </router-link>
+    <div class="p-3">
+      <h3 class="font-bold">{{ meal.strMeal }}</h3>
+      <p class="mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        iusto laborum eaque officia.
+      </p>
+      <div class="flex items-center justify-between">
+        <youtube-button :youtubeLink="meal.strYoutube">
+          Youtube
+        </youtube-button>
+        <router-link
+          :to="{ name: 'mealDetails', params: { id: meal.idMeal } }"
+          class="px-3 py-2 rounded border-2 text-white border-sky-500 bg-sky-500 hover:bg-sky-600 transition-colors"
+          >View</router-link
+        >
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import YoutubeButton from "../components/YoutubeButton.vue";
+const props = defineProps({
+  meal: {
+    required: true,
+    type: Object,
+  },
+});
+</script>
--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -14,40 +14,11 @@
     />
   </div>
   <div class="grid grid-cols-1 md:grid-cols-4 gap-5 p-8">
-    <div
-      v-for="(meal, index) in meals"
-      :key="index"
-      class="bg-white shadow rounded-xl"
-    >
-      <router-link :to="{ name: 'mealDetails', params: { id: meal.idMeal } }">
-        <img
-          :src="meal.strMealThumb"
-          :alt="meal.strMeal"
-          class="w-full h-48 object-cover rounded-t-2xl"
-        />
-      </router-link>
-      <div class="p-3">
-        <h3 class="font-bold">{{ meal.strMeal }}</h3>
-        <p class="mb-4">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
-          iusto laborum eaque officia.
-        </p>
-        <div class="flex items-center justify-between">
-          <youtube-button :youtubeLink="meal.strYoutube">
-            Youtube
-          </youtube-button>
-          <router-link
-            :to="{ name: 'mealDetails', params: { id: meal.idMeal } }"
-            class="px-3 py-2 rounded border-2 text-white border-sky-500 bg-sky-500 hover:bg-sky-600 transition-colors"
-            >View</router-link
-          >
-        </div>
-      </div>
-    </div>
+    <MealItem v-for="(meal, index) in meals" :key="index" :meal="meal" />
   </div>
 </template>
 <script setup>
-import YoutubeButton from "../components/YoutubeButton.vue";
+import MealItem from "../components/MealItem.vue";
 import { computed } from "@vue/reactivity";
 import { ref, onMounted } from "vue";
 import { useRoute } from "vue-router";
