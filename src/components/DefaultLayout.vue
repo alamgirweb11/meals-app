e3c0cc14e93447f2328d263875621365bef59bb9
alamgirweb11 <alamgirweb11@gmail.com>
2022-12-20 03:37:51 +0600

    update some css classes

--- src/components/DefaultLayout.vue
+++ src/components/DefaultLayout.vue
@@ -2,7 +2,9 @@
     <div class="bg-gray-100 h-full">
        <Navbar />
         <main>
+           <div class="max-w-[1200px] mx-auto">
             <router-view />
+           </div>
         </main>
     </div>
  
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,12 @@
 <template>
      <header class="bg-white shadow h-16 flex justify-between items-stretch">
             <div class="header-item-left">
-                <router-link :to="{name: 'home'}" class="inline-flex items-center h-full px-5">Home</router-link>
+                <router-link :to="{name: 'home'}" class="inline-flex items-center h-full px-5 text-orange-500 font-bold">Home</router-link>
             </div>
             <div class="header-item-right flex gap-1 items-center">
-                <router-link :to="{ name: 'mealsByName' }" class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100 ">Search Meals</router-link>
-                <router-link :to="{ name: 'mealsListByLetter' }" class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100 ">Meals By Letter</router-link>
-                <router-link :to="{ name: 'ingredients' }" class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100 ">Meals By Ingredients</router-link>
+                <router-link :to="{ name: 'mealsByName' }" class="inline-flex items-center px-2 h-full transition-colors hover:bg-orange-500 hover:text-white">Search Meals</router-link>
+                <router-link :to="{ name: 'mealsListByLetter' }" class="inline-flex items-center px-2 h-full transition-colors hover:bg-orange-500 hover:text-white">Meals By Letter</router-link>
+                <router-link :to="{ name: 'ingredients' }" class="inline-flex items-center px-2 h-full transition-colors hover:bg-orange-500 hover:text-white">Meals By Ingredients</router-link>
             </div>
         </header>
 </template>
